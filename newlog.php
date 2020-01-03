743bebbc7289f7df7ad54518f6b8ec86cd3832c4
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-01-03 18:14:41 +0530

    Update newlog.php

--- newlog.php
+++ newlog.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     
-    <form action="newlog.php" method="POST">
+    <form action="newlog.php" method="GET">
         Well Name: <input type="textbox" name="wellName"><br>
         Field Name: <input type="textbox" name="fieldName"><br>
         County: <input type="textbox" name="county"><br>
@@ -29,54 +29,63 @@
     </form>
 
     <?php
-        if (isset($_POST['wellname'])){
-            $wellName = $_POST['wellName'];
+
+
+        if (isset($_GET['wellName'])){
+            $wellName = $_GET['wellName'];
         }
 
-        if (isset($_POST['fieldName'])){
-            $fieldName = $_POST["fieldName"];
+
+        if (isset($_GET['fieldName'])){
+            $fieldName = $_GET["fieldName"];
         }
 
-        if (isset($_POST['county'])){
-            $county = $_POST["county"];
+        if (isset($_GET['county'])){
+            $county = $_GET["county"];
         }
 
-        if (isset($_POST['state'])){
-            $state = $_POST["state"];
+        if (isset($_GET['state'])){
+            $state = $_GET["state"];
         }
 
-        if (isset($_POST['scale'])){
-            $scale = $_POST["scale"];
+        if (isset($_GET['scale'])){
+            $scale = $_GET["scale"];
         }
-        if (isset($_POST['nameOfLog'])){
-            $nameofLog = $_POST["nameOfLog"];
-        }
-
-        if (isset($_POST['bli'])){
-            $bli = $_POST["bli"];
+        if (isset($_GET['nameOfLog'])){
+            $nameofLog = $_GET["nameOfLog"];
         }
 
-        if (isset($_POST['tli'])){
-            $tli = $_POST["tli"];
+        if (isset($_GET['bli'])){
+            $bli = $_GET["bli"];
         }
 
-        if (isset($_POST['remark'])){
-            $remark = $_POST["remark"];
+        if (isset($_GET['tli'])){
+            $tli = $_GET["tli"];
         }
 
-        if (isset($_POST['company'])){
-            $company = $_POST["company"];
+        if (isset($_GET['remark'])){
+            $remark = $_GET["remark"];
         }
 
-        if (isset($_POST['boxNo'])){
-            $boxNo = $_POST["boxNo"];
+        if (isset($_GET['company'])){
+            $company = $_GET["company"];
         }
 
-        if (isset($_POST['packetNo'])){
-            $packetNo = $_POST["packetNo"];
-        }   
-        
-        
+        if (isset($_GET['boxNo'])){
+            $boxNo = $_GET["boxNo"];
+        }
+
+        if (isset($_GET['packetNo'])){
+            $packetNo = $_GET["packetNo"];
+        }  
+        if($wellName!=NULL)
+        {
+        $sql = "INSERT INTO details (`Well Name`, `Field Name`, `State/Location`, `County`, `Scale`, `Name of Log`, `Bottom Logged Interval`, `Top Logged Interval`, `Remark`, `Company`, `Box No`, `Packet No`) VALUES ('$wellName', '$fieldName', '$state', '$county', '$scale', '$nameofLog', '$bli', '$tli', '$remark', '$company', '$boxNo', '$packetNo');";
+        if(mysqli_query($conn, $sql))
+            echo "Records inserted successfully.";
+        else
+            echo "ERROR: Could not able to execute $sql" . mysqli_connect_error($conn);
+        }
     ?>
 
 </body>
