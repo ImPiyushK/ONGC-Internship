ed136152e10e36764f88f63d7196394bc666da3d
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-01-10 17:35:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Index</title>
 </head>
 <body>
-    
+<?php
+    echo "Hello ".$_SESSION['userUid']."<br>";
+?>
+
     <a href="newlog.php">Create a new Log</a>
     <br>
     <a href="db.php">List of Table</a>
     <br>
     <a href="update.php">Update Log</a>
     <br>
-
+    <a href="delete.php">Delete Record</a>
+    <br>
+    <form action="includes/logout.inc.php" method="POST">
+        <button type="submit" name="logout-submit">Logout</button>
+    </form>
 </body>
 </html>
