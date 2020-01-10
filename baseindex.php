dbeaa3eb85bdfc83e7bc1e94084db07ef4b2282a
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-01-10 17:37:53 +0530

    Update baseindex.php

---
+++ baseindex.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+</head>
+<body>
+
+    <form action="includes/login.inc.php" method="POST">
+            <input type="text" name="mailuid" placeholder="Username/E-mail"><br>
+            <input type="password" name="pwd" placeholder="Password"><br>
+            <button type="submit" name="login-submit">Login</button>
+    </form>
+
+    <a href="signup.php">Signup</a>
+</body>
+</html>
