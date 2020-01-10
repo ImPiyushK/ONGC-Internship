2807a3504e2c98861cfe59b8e244f682cbae5134
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-01-10 17:42:53 +0530

    Update newlog.php

--- newlog.php
+++ newlog.php
@@ -1,18 +1,15 @@
-<?php  
-    include_once 'connect.php'
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create New Log</title>
+    <title>Update</title>
 </head>
 <body>
     
-    <form action="newlog.php" method="GET">
+    <form action="update.php" method="POST">
+        <input type="text" name="lid" placeholder="Enter LogID to Update"><br>
         Well Name: <input type="textbox" name="wellName"><br>
         Field Name: <input type="textbox" name="fieldName"><br>
         County: <input type="textbox" name="county"><br>
@@ -25,9 +22,9 @@
         Company: <input type="textbox" name="company"><br>
         Box No: <input type="textbox" name="boxNo"><br>
         Packet No: <input type="textbox" name="packetNo"><br>
-        <input type="Submit">
+        <button type="submit" name="signup-submit">Update</button>
     </form>
-
+    
     <?php
         $wellName = NULL;
         $fieldName = NULL;
@@ -41,63 +38,67 @@
         $company = NULL;
         $boxNo = NULL;
         $packetNo = NULL;
+        $id = NULL;
 
-        if (isset($_GET['wellName'])){
-            $wellName = $_GET['wellName'];
+        if (isset($_POST['lid'])){
+        $id = $_POST['lid'];
+        }
+
+        if (isset($_POST['wellName'])){
+            $wellName = $_POST['wellName'];
         }
 
 
-        if (isset($_GET['fieldName'])){
-            $fieldName = $_GET["fieldName"];
+        if (isset($_POST['fieldName'])){
+            $fieldName = $_POST["fieldName"];
         }
 
-        if (isset($_GET['county'])){
-            $county = $_GET["county"];
+        if (isset($_POST['county'])){
+            $county = $_POST["county"];
         }
 
-        if (isset($_GET['state'])){
-            $state = $_GET["state"];
+        if (isset($_POST['state'])){
+            $state = $_POST["state"];
         }
 
-        if (isset($_GET['scale'])){
-            $scale = $_GET["scale"];
+        if (isset($_POST['scale'])){
+            $scale = $_POST["scale"];
         }
-        if (isset($_GET['nameOfLog'])){
-            $nameofLog = $_GET["nameOfLog"];
-        }
-
-        if (isset($_GET['bli'])){
-            $bli = $_GET["bli"];
+        if (isset($_POST['nameOfLog'])){
+            $nameofLog = $_POST["nameOfLog"];
         }
 
-        if (isset($_GET['tli'])){
-            $tli = $_GET["tli"];
+        if (isset($_POST['bli'])){
+            $bli = $_POST["bli"];
         }
 
-        if (isset($_GET['remark'])){
-            $remark = $_GET["remark"];
+        if (isset($_POST['tli'])){
+            $tli = $_POST["tli"];
         }
 
-        if (isset($_GET['company'])){
-            $company = $_GET["company"];
+        if (isset($_POST['remark'])){
+            $remark = $_POST["remark"];
         }
 
-        if (isset($_GET['boxNo'])){
-            $boxNo = $_GET["boxNo"];
+        if (isset($_POST['company'])){
+            $company = $_POST["company"];
         }
 
-        if (isset($_GET['packetNo'])){
-            $packetNo = $_GET["packetNo"];
-        }  
-        if($wellName!=NULL)
+        if (isset($_POST['boxNo'])){
+            $boxNo = $_POST["boxNo"];
+        }
+
+        if (isset($_POST['packetNo'])){
+            $packetNo = $_POST["packetNo"];
+        }
+        if($id!=NULL)
         {
-        $sql = "INSERT INTO details (`Well Name`, `Field Name`, `State/Location`, `County`, `Scale`, `Name of Log`, `Bottom Logged Interval`, `Top Logged Interval`, `Remark`, `Company`, `Box No`, `Packet No`) VALUES ('$wellName', '$fieldName', '$state', '$county', '$scale', '$nameofLog', '$bli', '$tli', '$remark', '$company', '$boxNo', '$packetNo');";
+        $sql = "UPDATE `details` SET `Well Name` = '$wellName', `Field Name` = '$fieldName', `Name of Log` = '$nameofLog', `State/Location` = '$state', `County` = '$county', `Scale` = '$scale', `Bottom Logged Interval` = '$bli', `Top Logged Interval` = '$tli', `Remark` = '$remark', `Company` = '$company', `Box No` = '$boxNo', `Packet No` = '$packetNo' WHERE `LogID` = $id;";
         if(mysqli_query($conn, $sql))
-            echo "Records inserted successfully.";
+            echo "Records Updated successfully.";
         else
             echo "ERROR: Could not able to execute $sql" . mysqli_connect_error($conn);
         }
     ?>
-
 </body>
 </html>
