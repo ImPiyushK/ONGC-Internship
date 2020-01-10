016c3b596db2b6c37f54aec79c037322a7b9a761
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-01-10 17:45:35 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,6 +7,14 @@
     <title>Signup</title>
 </head>
 <body>
+
+    <form action="includes/signup.inc.php" method="POST">
+        <input type="text" name="uid" placeholder="Username">
+        <input type="text" name="mail" placeholder="E-Mail">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="password" name="pwd-confirm" placeholder="Confirm Password">
+        <button type="submit" name="signup-submit">SignUp</button>
+    </form>
     
 </body>
 </html>
